a379708578f3890bf89fea747eabe34f6da5e9e9
Jonathan Powell <jonathan_powell@brown.edu>
2018-05-10 19:49:15 -0400

    Database populated with dummy data

---
+++ server/populateData.js
@@ -0,0 +1,170 @@
+//GET ARTICLES
+
+//Post Methods
+// Add a dropdown field for sections on the article page.
+
+const fetch = require('node-fetch');
+function insertArticleIntoDatabase(params) {
+
+	fetch('http://localhost:9999/learn/article', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(params),
+	}).then((response => {
+		if (response.ok) {
+			console.log("Successfully inserted");
+		} else {
+			console.log("Unable to insert");
+		}
+	}));
+}
+
+function insertOpportunityIntoDatabase(params) {
+	console.log(params.type);
+	fetch('http://localhost:9999/opportunities', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(params),
+	}).then((response => {
+		if (response.ok) {
+			console.log("Successfully inserted Opportunity : ");
+		} else {
+			console.log("Unable to insert Opportunity");
+		}
+	}));
+}
+
+const sampleOpportunities =  [
+			{
+			title: "Opportunity Name",
+			type: 'How To Smoke',
+			organization: "Organization name",
+			compensation: "full-time, paid",
+			location: "Providence, RI",
+			deadline: "4/20/18",
+			description: "Maybe there was an old trapper that lived out here and maybe one day he went to check his beaver traps, and maybe he fell into the river and drowned. Everything's not great in life, but we can still find beauty in it. From all of us here, I want to wish you happy painting and God bless, my friends. Just relax and let it flow. That easy. I like to beat the brush. Let's put a touch more of the magic here. Let your heart be your guide. Even the worst thing we can do here is good. There comes a nice little fluffer. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart.",
+			qualifications: "Everyone wants to enjoy the good parts - but you have to build the framework first. If you don't think every day is a good day - try missing a few. You'll see. Let the paint work. A fan brush is a fantastic piece of equipment. Use it. Make friends with it. It's cold, but it's beautiful. Just go out and talk to a tree. Make friends with it. That's why I paint - because I can create the kind of world I want - and I can make this world as happy as I want it. I can't think of anything more rewarding than being able to express yourself to others through painting.",
+			commitment: "Exercising the imagination, experimenting with talents, being creative; these things, to me, are truly the windows to your soul. We want to use a lot pressure while using no pressure at all. Let's build an almighty mountain. These things happen automatically. All you have to do is just let them happen.",
+			howToApply: "Let's do it again then, what the heck. Let your heart take you to wherever you want to be. This is truly an almighty mountain. Use your imagination, let it go.",
+			about: "Just use the old one inch brush. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. I really recommend you use odorless thinner or your spouse is gonna run you right out into the yard and you'll be working by yourself. You got your heavy coat out yet? It's getting colder. We'll paint one happy little tree right here.",
+			link: "https://www.example.com/external-link",
+		},
+			{
+			title: "Opportunity Name",
+			type: 'How To Smoke',
+			organization: "Organization name",
+			compensation: "full-time, paid",
+			location: "Providence, RI",
+			deadline: "4/20/18",
+			description: "Maybe there was an old trapper that lived out here and maybe one day he went to check his beaver traps, and maybe he fell into the river and drowned. Everything's not great in life, but we can still find beauty in it. From all of us here, I want to wish you happy painting and God bless, my friends. Just relax and let it flow. That easy. I like to beat the brush. Let's put a touch more of the magic here. Let your heart be your guide. Even the worst thing we can do here is good. There comes a nice little fluffer. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart.",
+			qualifications: "Everyone wants to enjoy the good parts - but you have to build the framework first. If you don't think every day is a good day - try missing a few. You'll see. Let the paint work. A fan brush is a fantastic piece of equipment. Use it. Make friends with it. It's cold, but it's beautiful. Just go out and talk to a tree. Make friends with it. That's why I paint - because I can create the kind of world I want - and I can make this world as happy as I want it. I can't think of anything more rewarding than being able to express yourself to others through painting.",
+			commitment: "Exercising the imagination, experimenting with talents, being creative; these things, to me, are truly the windows to your soul. We want to use a lot pressure while using no pressure at all. Let's build an almighty mountain. These things happen automatically. All you have to do is just let them happen.",
+			howToApply: "Let's do it again then, what the heck. Let your heart take you to wherever you want to be. This is truly an almighty mountain. Use your imagination, let it go.",
+			about: "Just use the old one inch brush. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. I really recommend you use odorless thinner or your spouse is gonna run you right out into the yard and you'll be working by yourself. You got your heavy coat out yet? It's getting colder. We'll paint one happy little tree right here.",
+			link: "https://www.example.com/external-link",
+		},
+			{
+			title: "Opportunity Name",
+			type: 'How To Smoke',
+			organization: "Organization name",
+			compensation: "full-time, paid",
+			location: "Providence, RI",
+			deadline: "4/20/18",
+			description: "Maybe there was an old trapper that lived out here and maybe one day he went to check his beaver traps, and maybe he fell into the river and drowned. Everything's not great in life, but we can still find beauty in it. From all of us here, I want to wish you happy painting and God bless, my friends. Just relax and let it flow. That easy. I like to beat the brush. Let's put a touch more of the magic here. Let your heart be your guide. Even the worst thing we can do here is good. There comes a nice little fluffer. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart.",
+			qualifications: "Everyone wants to enjoy the good parts - but you have to build the framework first. If you don't think every day is a good day - try missing a few. You'll see. Let the paint work. A fan brush is a fantastic piece of equipment. Use it. Make friends with it. It's cold, but it's beautiful. Just go out and talk to a tree. Make friends with it. That's why I paint - because I can create the kind of world I want - and I can make this world as happy as I want it. I can't think of anything more rewarding than being able to express yourself to others through painting.",
+			commitment: "Exercising the imagination, experimenting with talents, being creative; these things, to me, are truly the windows to your soul. We want to use a lot pressure while using no pressure at all. Let's build an almighty mountain. These things happen automatically. All you have to do is just let them happen.",
+			howToApply: "Let's do it again then, what the heck. Let your heart take you to wherever you want to be. This is truly an almighty mountain. Use your imagination, let it go.",
+			about: "Just use the old one inch brush. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. I really recommend you use odorless thinner or your spouse is gonna run you right out into the yard and you'll be working by yourself. You got your heavy coat out yet? It's getting colder. We'll paint one happy little tree right here.",
+			link: "https://www.example.com/external-link",
+		},
+			{
+			title: "Opportunity Name",
+			type: 'How To Smoke',
+			organization: "Organization name",
+			compensation: "full-time, paid",
+			location: "Providence, RI",
+			deadline: "4/20/18",
+			description: "Maybe there was an old trapper that lived out here and maybe one day he went to check his beaver traps, and maybe he fell into the river and drowned. Everything's not great in life, but we can still find beauty in it. From all of us here, I want to wish you happy painting and God bless, my friends. Just relax and let it flow. That easy. I like to beat the brush. Let's put a touch more of the magic here. Let your heart be your guide. Even the worst thing we can do here is good. There comes a nice little fluffer. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart.",
+			qualifications: "Everyone wants to enjoy the good parts - but you have to build the framework first. If you don't think every day is a good day - try missing a few. You'll see. Let the paint work. A fan brush is a fantastic piece of equipment. Use it. Make friends with it. It's cold, but it's beautiful. Just go out and talk to a tree. Make friends with it. That's why I paint - because I can create the kind of world I want - and I can make this world as happy as I want it. I can't think of anything more rewarding than being able to express yourself to others through painting.",
+			commitment: "Exercising the imagination, experimenting with talents, being creative; these things, to me, are truly the windows to your soul. We want to use a lot pressure while using no pressure at all. Let's build an almighty mountain. These things happen automatically. All you have to do is just let them happen.",
+			howToApply: "Let's do it again then, what the heck. Let your heart take you to wherever you want to be. This is truly an almighty mountain. Use your imagination, let it go.",
+			about: "Just use the old one inch brush. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. I really recommend you use odorless thinner or your spouse is gonna run you right out into the yard and you'll be working by yourself. You got your heavy coat out yet? It's getting colder. We'll paint one happy little tree right here.",
+			link: "https://www.example.com/external-link",
+		},
+		];
+
+const sampleArticles = [
+			{
+				id: '420',
+				image: '/asset/article-abc.jpg',
+				title: 'Newest article',
+				author: 'Rotomoto',
+				date: '4/20/2018',
+				url: '',
+				summary: 'I started painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. This painting comes right out of your heart.',
+				source: '',
+				section: 'How To Smoke'
+			},
+			{
+				id: '420',
+				image: '/asset/article-abc.jpg',
+				title: 'Article on SMART',
+				author: 'Rotomoto',
+				date: '4/20/2018',
+				url: '',
+				summary: 'I started painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. This painting comes right out of your heart.',
+				source: '',
+				section: 'How To Smoke'
+			},
+			{
+				id: '420',
+				image: '/asset/article-abc.jpg',
+				title: 'External Article',
+				author: 'Rotomoto',
+				date: '4/20/2018',
+				url: 'https://www.example.com/external-link',
+				summary: 'I started painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. This painting comes right out of your heart.',
+				source: 'PR0HBTD',
+				section: 'How To Smoke',
+			},
+			{
+				id: '420',
+				image: '/asset/article-abc.jpg',
+				title: 'How to properly inhale IV',
+				author: 'Rotomoto',
+				date: '4/20/2018',
+				url: 'https://www.example.com/external-link',
+				summary: 'I started painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. This painting comes right out of your heart.',
+				source: 'PR0HBTD',
+				section: 'How To Smoke',
+			},
+			{
+				id: '420',
+				image: '/asset/article-abc.jpg',
+				title: 'How to properly inhale V',
+				author: 'Rotomoto',
+				date: '4/20/2018',
+				url: 'https://www.example.com/external-link',
+				summary: 'I started painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. This painting comes right out of your heart.',
+				source: 'PR0HBTD',
+				section: 'How To Smoke',
+			},
+			{
+				id: '420',
+				image: '/asset/article-abc.jpg',
+				title: 'How to properly inhale VI',
+				author: 'Rotomoto',
+				date: '4/20/2018',
+				url: 'https://www.example.com/external-link',
+				summary: 'I started painting as a hobby when I was little. I didn\'t know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. This painting comes right out of your heart.',
+				source: 'PR0HBTD',
+				section: 'How To Smoke',
+			},
+		];
+
+sampleArticles.map(insertArticleIntoDatabase);
+sampleOpportunities.map(insertOpportunityIntoDatabase);
--- server/server.js
+++ server/server.js
@@ -13,19 +13,19 @@ const connPool = anyDB.createPool(dbURL, {min: 0, max: 50});
 
 /* Sections Table */
 connPool.query('CREATE TABLE IF NOT EXISTS section(' +
-	'section_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+	'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
 	'section TEXT)');
 
 /* Chapters Table */
 connPool.query('CREATE TABLE IF NOT EXISTS chapter(' +
-	'chapter_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+	'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
  'name TEXT,' +
  'email TEXT,' + 
  'university TEXT)');
 
 /* User Table */
 connPool.query('CREATE TABLE IF NOT EXISTS user(' +
-	'user_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+	'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
  'username TEXT,' +
  'email TEXT,' +
  'created DATETIME,' + 
@@ -34,33 +34,29 @@ connPool.query('CREATE TABLE IF NOT EXISTS user(' +
  'isOrg BOOLEAN)');
 
 /* Article Table */
-connPool.query('CREATE TABLE IF NOT EXISTS article(article_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
- 'isApproved BOOLEAN,' + 
+connPool.query('CREATE TABLE IF NOT EXISTS article(id INTEGER PRIMARY KEY AUTOINCREMENT,' +
  'title TEXT,' + 
  'content TEXT,' + 
- 'submitted DATETIME,' + 
+ 'date DATETIME,' + 
  'section TEXT,' + 
- 'username TEXT,' + 
- 'image_url TEXT,' + 
- 'source TEXT,' +
- 'FOREIGN KEY (section) REFERENCES section(section),' +
- 'FOREIGN KEY (username) REFERENCES user(username))');
+ 'author TEXT,' + 
+ 'image TEXT,' + 
+ 'source TEXT)');
 
 /* Opportunities Table*/
-connPool.query('CREATE TABLE IF NOT EXISTS opportunity(opportunity_id INTEGER PRIMARY KEY AUTOINCREMENT,' + 
-	'isApproved BOOLEAN,' + 
-	'about TEXT,' + 
-	'commitment TEXT,' + 
-	'compensation INTEGER,' +
+connPool.query('CREATE TABLE IF NOT EXISTS opportunity(id INTEGER PRIMARY KEY AUTOINCREMENT,' + 
+	'type TEXT,' +
+	'title TEXT,' +
+	'organization TEXT,' +
+	'compensation TEXT,' +
+	'location TEXT,' + 
+	'deadline TEXT,' +
 	'description TEXT,' + 
-	'location TEXT,' +
-	'deadline DATETIME,' + 
-	'qualifications TEXT,' + 
-	'how TEXT,' + 
-	'title TEXT,' + 
-	'link TEXT,' +
-	'organization,' +
-	'FOREIGN KEY(organization) REFERENCES user(username))'); 
+	'qualifications TEXT, ' + 
+	'committment TEXT, ' +
+	'howToApply TEXT, ' +
+	'about TEXT,' +
+	'link TEXT)'); 
 
 
 /** LEARN PAGE **/
@@ -68,7 +64,7 @@ connPool.query('CREATE TABLE IF NOT EXISTS opportunity(opportunity_id INTEGER PR
 //GET ALL SECTIONS
 app.get('/learn/sections', function (request, response) {
 	console.log("GET ALL SECTIONS REQUEST");
-	const sectionQuery = 'SELECT DISTINCT section FROM section;';
+	const sectionQuery = 'SELECT DISTINCT section FROM article ORDER BY section;';
 	const allSections = connPool.query(sectionQuery, function(error, data) {
 		if (error) {
 			response.send( {success: false, data: []})
@@ -80,29 +76,28 @@ app.get('/learn/sections', function (request, response) {
 //GET ALL ARTICLES GIVEN A SECTION
 app.get('/learn/sections/:section', function (request, response) {
 	console.log("GET ALL ARTICLES FOR A GIVEN SECTION");
-	const sectionArticles = 'SELECT * FROM articles WHERE section IS $1;';
+	const sectionArticles = 'SELECT * FROM article WHERE section IS $1;';
+	console.log(data)
 	connPool.query(sectionArticles, [request.body.section], function (error, data) {
+		console.log(data);
 		if (error) {
 			response.send( {success: false, data: []})
 		}
 		response.send({success: true, data: data.rows});
-	})
+	});
 });
 
 //GET ARTICLE GIVEN ARTICLE ID
 app.get('/learn/:articleid', function (request, response) {
 	console.log("GET AN ARTICLE GIVEN AN ARTICLE ID");
-	const articleQuery = 'SELECT * FROM articles WHERE article_id = $1;';
+	const articleQuery = 'SELECT * FROM article WHERE id = $1;';
 	connPool.query(articleQuery, [request.body.articleid], function (error, data) {
 		if (error) {
 			response.send( {success: false, data: []})
 		}
 		response.send({success: true, data: data.rows});
-	})
+	});
 });
-
-//GET ARTICLES
-
 
 //POST ARTICLE FROM FORM
 app.post('/learn/article', function (request, response) {
@@ -110,24 +105,26 @@ app.post('/learn/article', function (request, response) {
 	const params = request.body;
 	const article = [
 		null, //autoincrement id
-		false, //isApproved
 		params.title,
 		params.content,
 		Date.now(),
 		params.section,
-		params.user_id,
-		params.image_url,
-		params.source
+		params.author,
+		params.image,
+		params.source,
 	];
-
-	const insertArticleQuery = 'INSERT into article VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+	console.log("Inserting Article: " + article  + " into the database");
+	const insertArticleQuery = 'INSERT into article VALUES($1, $2, $3, $4, $5, $6, $7, $8)';
 	connPool.query(insertArticleQuery, article, function(error, data) {
 		if (error) {
+			console.error(error);
 			response.send({success: false});
-		} 
+		}  else {
+		console.log("Article succesfully inserted");
 		response.send({success: true});
+			
+		}
 	});
-
 });
 
 
@@ -142,34 +139,38 @@ Connect Page
 */
 
 app.post('/opportunities', function(request, response) {
-	const params = response.body;
+	const params = request.body;
 	const opportunity = [null, //id (AUTOINCREMENT)
-	false, //isApproved
-	params.about,  
-	params.commitment, 
+	params.type,
+	params.title,
+	params.organization,
 	params.compensation, 
+	params.location,
 	params.deadline,
 	params.description,
-	params.location,
 	params.qualifications,
-	params.how,
-	params.organization,
-	params.title,
+	params.committment,
+	params.howToApply,
+	params.about,
 	params.link,
-	 ];
+	];
 
-	const insertOppprtunityQuery = 'INSERT into opportunity VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, 13)';
+	const insertOppprtunityQuery = 'INSERT into opportunity VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)';
 	connPool.query(insertOppprtunityQuery, opportunity, function(error, data) {
 		if (error) {
+			console.error(error);
 			response.send({success: false});
+		} else {
+			console.log("Successfully added opportunity to database.");
+			response.send({success: true});
 		}
-		response.send({success: true});
 	});
 
 });
 
 
 app.get('/connect', function(request, response) {
+	console.log("Request recieved for all opportunities");
 	const opportunitiesQuery = 'SELECT * FROM opportunity;';
 	connPool.query(opportunitiesQuery, function (error, data) {
 		if (error) {
@@ -183,7 +184,7 @@ app.get('/connect', function(request, response) {
 //GET opportunities given an organization
 app.get('/connect/:organization_name', function(request, response) {
 	const params = request.body;
-	const opportunitiesQuery = 'SELECT * FROM opportunity WHERE opportunity_id IS $1';
+	const opportunitiesQuery = 'SELECT * FROM opportunity WHERE id IS $1';
 	connPool.query(opportunitiesQuery, [params.organization], function (error, data) {
 		if (error) {
 			response.send( {success: false, data: []})
@@ -196,7 +197,7 @@ app.get('/connect/:organization_name', function(request, response) {
 app.get('/connect/opportunities/:id', function (request, response) {
 	console.log("GET AN OPPORTUNITY GIVEN AN ID");
 	const params = request.body;
-	const opportunityQuery = 'SELECT * FROM opportunity WHERE opportunity_id IS $1';
+	const opportunityQuery = 'SELECT * FROM opportunity WHERE id IS $1';
 
 	connPool.query(opportunityQuery, [params.id], function (request, response) {
 		if (error) {
@@ -206,12 +207,6 @@ app.get('/connect/opportunities/:id', function (request, response) {
 		response.send({success: true, data: data.rows});
 	});
 });
-
-//Post Methods
-
-
-
-
 
 
 // --- LISTEN ON PORT 9999 --- //
